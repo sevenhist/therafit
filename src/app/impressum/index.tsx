bf2fa8e4b33765f24b5b9618a063ae93efb8f45f
alexanderleitner0 <alexander.leitner0@bulme.at>
2025-02-04 18:17:50 +0100

    added text for impressum

--- src/app/impressum/index.tsx
+++ src/app/impressum/index.tsx
@@ -1,7 +1,26 @@
-import s from "./Impressum.module.scss"
+import s from "./Impressum.module.scss";
 
 export const Impressum = () => {
     return (
-        <div className={s.impressum}>Impressum</div>
-    )
-}
+            <div className={s.impressum}>
+                <h1>Impressum</h1>
+                
+                <h2>Adresse:</h2>
+                <p>Ibererstraße 15 – 21</p>
+                <p>8051 Graz</p>
+                
+                <h2>E-Mail:</h2>
+                <p>willkommen@bulme.at</p>
+
+                <h2>Telefon:</h2>
+                <p>+43 50 248 066-0</p>
+
+                <h2>Geschäftsführung:</h2>
+                <p>Alexander Leitner und Dmytro Rudnenko</p>
+
+                <p>Diese Website ist ein Open-Source-Projekt.</p>
+                <p>Alle Inhalte stehen kostenlos zur Verfügung.</p>
+            </div>
+        
+    );
+};
