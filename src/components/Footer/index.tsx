c8d4a6f38d414abde0921f00258647904616084e
alexanderleitner0 <alexander.leitner0@bulme.at>
2025-02-04 11:09:18 +0100

    footer final

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,9 +2,47 @@ import { FC } from "react"
 import s from "./Footer.module.scss"
 
 export const Footer: FC = () => {
+    
+    const navLinks = [
+        'ABOUT US',
+        'CONTACT US',
+        'PRIVACY POLICY'
+    ];
+
     return (
         <footer className={s.footer}>
-            <div>Footer</div>
+            <div className={s.footer__container}>
+                <div className={s.footer__logo}>
+                    <div className={s.footer__logo_box}>FitData</div>
+                </div>
+
+                <div className={s.footer__info}>
+                    <div className={s.footer__contact}>
+                        <div className={s.footer__address}>
+                            <i className={s.footer__icon}>📍</i>
+                            15-21 Ibererstrasse • Graz, ST, 8051
+                        </div>
+                        <div className={s.footer__contacts}>
+                            <span>
+                                <i className={s.footer__icon}>📞</i>
+                                (050) 248-0660
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <nav className={s.footer__nav}>
+                    {navLinks.map((link) => (
+                        <a key={link} href="#" className={s.footer__link}>
+                            {link}
+                        </a>
+                    ))}
+                </nav>
+
+                <div className={s.footer__copyright}>
+                    Copyright © 2025 • FitData
+                </div>
+            </div>
         </footer>
     )
 }
