e6ce979f44b3a3b68042b20041deb030ddaa4ccc
alexanderleitner0 <alexander.leitner0@bulme.at>
2025-02-05 09:01:03 +0100

    deleted impressum folder and first idea for legal notice

--- src/app/legal-notice/index.tsx
+++ src/app/legal-notice/index.tsx
@@ -1,7 +1,26 @@
-import s from "./LegalNotice.module.scss"
+import s from "./LegalNotice.module.scss";
 
 export const LegalNotice = () => {
     return (
-        <div className={s.legalNotice}>Legal Notice</div>
-    )
-}
+            <div className={s.legalNotice}>
+                <h1>Legal Notice</h1>
+                
+                <h2>Address</h2>
+                <p>Ibererstraße 15 – 21</p>
+                <p>8051 Graz</p>
+                
+                <h2>E-Mail:</h2>
+                <p>willkommen@bulme.at</p>
+
+                <h2>Telefon:</h2>
+                <p>+43 50 248 066-0</p>
+
+                <h2>Management:</h2>
+                <p>Alexander Leitner and Dmytro Rudnenko</p>
+
+                <p>This website is an open source project.</p>
+                <p>All content is available free of charge.</p>
+            </div>
+        
+    );
+};
