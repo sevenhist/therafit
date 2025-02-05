cd86655265a3e88c9068b450c5c23cdff93349a2
Rudnenko Dmytro <sevenhist@gmail.com>
2025-02-05 08:38:21 +0100

    legal notice umbenehmen

--- src/app/impressum/index.tsx
+++
@@ -1,7 +0,0 @@
-import s from "./Impressum.module.scss"
-
-export const Impressum = () => {
-    return (
-        <div className={s.impressum}>Impressum</div>
-    )
-}
---
+++ src/app/legal-notice/index.tsx
@@ -0,0 +1,7 @@
+import s from "./LegalNotice.module.scss"
+
+export const LegalNotice = () => {
+    return (
+        <div className={s.legalNotice}>Legal Notice</div>
+    )
+}
