f25d6c5feada15dfbd282f535b359befeb96bcbe
Rudnenko Dmytro <sevenhist@gmail.com>
2025-02-06 19:33:43 +0100

    next.js changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'export', // Включаем режим статического экспорта
-  images: {
-    unoptimized: true, // Отключаем оптимизацию изображений (некоторые старые методы могут не поддерживаться)
-  },
-  trailingSlash: true, // Опционально, если хочешь, чтобы все URL заканчивались слэшем, например, `/about/`
-};
-
-module.exports = nextConfig;
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
