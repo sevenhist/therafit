63bc0bfe65409b6010dd8642f766ab50c6ad59da
Rudnenko Dmytro <sevenhist@gmail.com>
2025-02-06 19:23:04 +0100

    editing next.congih.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Включаем режим статического экспорта
+  images: {
+    unoptimized: true, // Отключаем оптимизацию изображений (некоторые старые методы могут не поддерживаться)
+  },
+  trailingSlash: true, // Опционально, если хочешь, чтобы все URL заканчивались слэшем, например, `/about/`
 };
 
-export default nextConfig;
+module.exports = nextConfig;
