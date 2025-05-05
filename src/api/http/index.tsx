9982389bad9efa539a06c25e25a966d59fb347b3
Rudnenko Dmytro <sevenhist@gmail.com>
2025-05-05 17:49:31 +0200

    neue änderungen

--- src/api/http/index.tsx
+++ src/api/http/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = `http://localhost:3001/api`
+export const API_URL = `https://therafit-backend.up.railway.app/api`
 
 const $api = axios.create({
     withCredentials: true,
