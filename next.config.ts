6217de651ba748a5fd4327c9c96c879eb45f26de
Rudnenko Dmytro <sevenhist@gmail.com>
2025-05-06 08:53:58 +0200

    pwa

--- next.config.ts
+++ next.config.ts
@@ -7,4 +7,12 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public', // Der Ordner, in dem die PWA-Dateien gespeichert werden (normalerweise 'public')
+  register: true, // Automatische Registrierung des Service Workers aktivieren
+  skipWaiting: true, // Den neuen Service Worker sofort aktivieren, ohne auf den Benutzer zu warten
+  disable: process.env.NODE_ENV === 'development', // PWA im Entwicklungsmodus deaktivieren
+  // Weitere Optionen kannst du hier hinzufügen (siehe Dokumentation)
+})
+
+module.exports = withPWA(nextConfig);
